63b35a1f1e0ca62532abc8b5f8e68143ce9153e6
niayesh <z.niayeshi@pinket.com>
2024-12-20 13:36:24 +0100

    Initial MVVM with clean architecture

--- app/src/main/java/com/example/app_core/ui/Type.kt
+++ app/src/main/java/com/example/app_core/ui/Type.kt
@@ -1,4 +1,4 @@
-package com.example.app_core.ui.theme
+package com.example.app_core.ui
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "app_core"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":presentation")
